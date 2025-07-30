e195d7968ecdfebe2c0b6863cba167f4fd2e8728
Luis Gustavo <luisgustavomarilia52@gmail.com>
2025-07-30 20:55:37 -0300

    iniciO do projeto calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,15 @@
+namespace calculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
